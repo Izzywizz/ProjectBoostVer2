ad8f69a38d62c55b75510dbcc0b84fafad5805fa
Isfand <isfandyar.ali@me.com>
2020-05-13 08:57:05 +0100

    Added a skelaton framework for the processing the input, changed the folder structures so its a bit neater

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    /// Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    /// Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+
+    /// <summary>
+    /// Handles Rotation and Thrust input
+    /// </summary>
+    private void ProcessInput()
+    {
+        // Thrust
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Thrust");
+        }
+
+        // Rotation
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotating Right");
+        }
+    }
+}
